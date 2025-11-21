fcf0c1c8433067b416711e6a283a0c2f66ba3e77
farhanyassar2003 <farhanyassar2003@gmail.com>
2025-11-21 08:24:54 +0530

    small change

--- Velora_Project/settings.py
+++ Velora_Project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
